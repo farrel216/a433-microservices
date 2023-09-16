f5ecb057cc92250cd81493b74225d2556d72298c
farrel216 <farrel.arp@gmail.com>
2023-09-17 02:16:05 +0700

    executable buid and push image

---
+++ build_push_image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image item-app, dan memiliki tag v1.
+docker build -t item-app:v1 .
+# Melihat daftar image di lokal.
+docker image ls -a
+# Mengubah nama image agar sesuai dengan format Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker tag item-app:v1 ghcr.io/farrel216/item-app:v1
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal.
+echo $CR_PAT | docker login ghcr.io -u farrel216 --password-stdin
+# Mengunggah image ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker push ghcr.io/farrel216/item-app:v1
