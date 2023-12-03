1dfb6b98c060f3be4cd0083b781a86a3a6ed4a54
farrel216 <farrel.arp@gmail.com>
2023-12-03 11:13:41 +0700

    feat: add build push image script

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Perintah untuk membuat Docker image dari Dockerfile yang tadi dibuat, dengan nama image karsajobs-ui:latest.
+docker build -t ghcr.io/farrel216/karsajobs-ui:latest .
+# Login ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat) via Terminal.
+echo $GHCR_PAT | docker login ghcr.io -u farrel216 --password-stdin
+# Mengunggah image ke Docker Hub (atau GitHub Packages bila menerapkan saran keempat).
+docker push ghcr.io/farrel216/karsajobs-ui:latest
